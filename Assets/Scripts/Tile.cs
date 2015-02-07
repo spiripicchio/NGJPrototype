d5cdfcbfebf3ddaa901378aaf4f259d6e6ecc19b
Marco Bancale <etabubu@h209.aau-1-day-2a.wlan.aalborguniversitet.dk>
2015-02-07 14:18:29 +0100

    Tiles and map

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour 
+{
+	public enum TileType
+	{
+		Normal,
+		Pit,
+		Bomb
+	}
+
+	public TileType type;
+
+	// Use this for initialization
+	void Start() 
+	{
+	
+	}
+}
